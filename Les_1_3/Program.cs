7c06ca525cec37454a3b71a7af1c3237c74f98c8
марта <kukovamk@gmail.com>
2023-03-27 20:49:43 +0300

    Les_1_3

---
+++ Les_1_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одомерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке[10, 99].
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+void RetNum(int[] Mass, int from, int to)
+{
+    int count = 0;
+    
+    for (int i = 0; i < Mass.Length; i++)
+    {
+        if (Mass[i] >= 10 & Mass[i] <= 99)
+        count = count + 1;
+         
+    }
+    return count;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+
+int start1 = int.Parse(Console.ReadLine()!);
+int stop1 = int.Parse(Console.ReadLine()!);
+
+int result = RetNum(mass, start1, stop1);
+Console.WriteLine(result);
